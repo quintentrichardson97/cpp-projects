f7f972869b4a1480217a3b2183eff2a06421579b
Quinten Richardson <quintentrichardson@gmail.com>
2026-05-12 02:24:36 -0500

    learncpp questions from 2.4

---
+++ doubledvalue.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int doubledNumber(int x)
+{
+
+  return x * 2;
+
+}
+
+//Program that takes the users input number and doubles it
+int main()
+{
+  std::cout << "Please input a number you would like doubled: ";
+  
+  int myNum{};
+  
+  std::cin >> myNum;
+
+  std::cout << "Your number doubled is " << doubledNumber(myNum) << "\n"; 
+  
+  return 0;
+}
