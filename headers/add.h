3fd8c194dfec61db134cbef8c18dd51213d348e9
Quinten Richardson <quintentrichardson@gmail.com>
2026-05-14 04:32:23 -0500

    cleaned up the add.h files to not include course notes and to also include header guards that I somehow missed!

--- headers/add.h
+++ headers/add.h
@@ -1,4 +1,7 @@
-// We really should have a header guard here, but will omit it for simplicity (we'll cover header guards in the next lesson)
+#ifndef ADD_H
+#define ADD_H
 
-// This is the content of the .h file, which is where the declarations go
-int add(int x, int y); // function prototype for add.h -- don't forget the semicolon!
+int add(int x, int y);
+
+#endif // !ADD_H
+
